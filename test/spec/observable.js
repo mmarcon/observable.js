487e93f8fde34a48c539b8420f3fdeb47f61ab7c
Massimiliano Marcon <me@marcon.me>
2013-01-25 22:12:39 +0100

    Added unit tests to verify what happens with nested models

--- test/spec/observable.js
+++ test/spec/observable.js
@@ -19,9 +19,9 @@
  */
 
 
-describe('Observable', function(){
+describe('Observable.js', function(){
 
-    beforeEach(function(){
+    function mockXMLHttpRequestWithResponseText(responseText) {
         window.XMLHttpRequest = function(){
             this.open = jasmine.createSpy('XMLHttpRequest open');
             this.send = function(){
@@ -30,99 +30,273 @@ describe('Observable', function(){
             this.readyState = 4;
             this.status = 200;
             this.getResponseHeader = jasmine.createSpy('XMLHttpRequest getResponseHeader').andReturn('application/json');
-            this.responseText = '{"response":{"name":"Max","age":31,"language":["italian","english"]},"meta":{"status":200}}';
+            this.responseText = responseText;
         };
+    }
+
+    describe('Model', function(){
+        var cachedXMLHttpRequest = window.XMLHttpRequest;
+
+        beforeEach(function(){
+            mockXMLHttpRequestWithResponseText('{"response":{"name":"Max","age":31,"language":["italian","english"]},"meta":{"status":200}}');
+        });
+
+        afterEach(function(){
+            window.XMLHttpRequest = cachedXMLHttpRequest;
+        });
+
+        it('Checks everything is defined', function(){
+            expect(OO).toBeDefined();
+            expect(OO.Model).toBeDefined();
+            expect(OO.Observer).toBeDefined();
+        });
+
+        it('Creates a Model', function(){
+            var person = {
+                name: 'Max',
+                age: 30,
+                language: ['italian', 'english']
+            };
+            var model = OO.Model({model: person});
+
+            expect(model instanceof OO.Model).toBe(true);
+            expect(model._state).toBeDefined();
+            expect(model.name).toBe('Max');
+            expect(model.age).toBe(30);
+            expect(model.language.length).toBe(2);
+        });
+
+        it('Triggers events on Models', function(){
+            var changeCallback = jasmine.createSpy('Something changed!');
+
+            var person = {
+                name: 'Max',
+                age: 30,
+                language: ['italian', 'english']
+            };
+            var model = OO.Model({model: person});
+            var cbid = model.on('change', changeCallback);
+            model.age = 31;
+
+            expect(changeCallback).toHaveBeenCalledWith('change', 'age');
+            changeCallback.reset();
+
+            model.language = ['italian', 'english', 'one day german'];
+
+            expect(changeCallback).toHaveBeenCalledWith('change', 'language');
+            changeCallback.reset();
+
+            model.off('change', cbid);
+            model.age = 30;
+
+            expect(changeCallback).not.toHaveBeenCalled();
+        });
+
+        it('Refreshes the model by contacting the provided data source', function(){
+            var changeCallback = jasmine.createSpy('Something changed!');
+
+            var person = {
+                name: 'Max',
+                age: 30,
+                language: ['italian', 'english']
+            };
+            var model = OO.Model({
+                model: person,
+                source: '/person/1234',
+                mapper: function(data){
+                    //Response is of type
+                    //{resonse: {my person}, meta:{info}}
+                    return data.response;
+                }});
+            var cbid = model.on('change', changeCallback);
+
+            model.refresh();
+
+            expect(changeCallback.callCount).toBe(3);
+        });
+
+        it('Refreshes the model by contacting the new data source when the source is updated', function(){
+            var changeCallback = jasmine.createSpy('Something changed!');
+            mockXMLHttpRequestWithResponseText('{"response":{"name":"John","age":25,"language":["english", "spanish"]},"meta":{"status":200}}');
+
+            var person = {
+                name: 'Max',
+                age: 30,
+                language: ['italian', 'english']
+            };
+            var model = OO.Model({
+                model: person,
+                source: '/person/1234',
+                mapper: function(data){
+                    //Response is of type
+                    //{resonse: {my person}, meta:{info}}
+                    return data.response;
+                }});
+            var cbid = model.on('change', changeCallback);
+
+            model.source = '/person/456';
+
+            expect(changeCallback.callCount).toBe(3);
+        });
     });
 
-    it('Checks everything is defined', function(){
-        expect(OO).toBeDefined();
-        expect(OO.Model).toBeDefined();
-        expect(OO.Observer).toBeDefined();
+    describe('Nested models', function(){
+        it('Works with nested models', function(){
+            var pChangeCallback = jasmine.createSpy('Something changed in person!');
+            var hChangeCallback = jasmine.createSpy('Something changed in hobbies!');
+
+            var hobbies = {
+                monday: ['football', 'reading'],
+                wednesday: ['free climbing'],
+                friday: ['cooking classes']
+            };
+
+            var hModel = OO.Model({model: hobbies});
+
+            var person = {
+                name: 'Max',
+                age: 30,
+                hobby: hModel
+            };
+
+            var pModel = OO.Model({
+                model: person
+            });
+
+            //Getter chain
+            expect(pModel.hobby.monday.length).toBe(2);
+            expect(pModel.hobby.monday[0]).toBe('football');
+
+            //Events
+            pModel.on('change', pChangeCallback);
+            hModel.on('change', hChangeCallback);
+
+            hModel.monday = ['swimming'];
+            expect(hChangeCallback).toHaveBeenCalledWith('change', 'monday');
+            expect(pChangeCallback).not.toHaveBeenCalled();
+        });
+
+        it('Works with AJAX and nested models', function(){
+            var pChangeCallback = jasmine.createSpy('Something changed in person!');
+            var hChangeCallback = jasmine.createSpy('Something changed in hobbies!');
+
+            mockXMLHttpRequestWithResponseText('{"response":{"hobbies": [{"day":"monday", "hobby":"free climbing"}]}, "meta":{"status": 200}}');
+
+            var hobbies = [
+                {
+                    day: 'monday',
+                    hobby: 'swimming'
+                },
+                {
+                    day: 'wednesday',
+                    hobby: 'football'
+                }
+            ];
+
+            var hModel = OO.Model({
+                model: {list: hobbies},
+                mapper: function(data){
+                    return {
+                        list: data.response.hobbies
+                    };
+                },
+                source: '/person/1234/hobbies'
+            });
+
+            var person = {
+                name: 'Max',
+                age: 30,
+                hobby: hModel
+            };
+
+            var pModel = OO.Model({
+                model: person
+            });
+
+            //Getter chain
+            expect(pModel.hobby.list.length).toBe(2);
+            expect(pModel.hobby.list[0].hobby).toBe('swimming');
+
+            //Events
+            pModel.on('change', pChangeCallback);
+            hModel.on('change', hChangeCallback);
+
+            //Refresh from server
+            hModel.refresh();
+
+            expect(hChangeCallback).toHaveBeenCalledWith('change', 'list');
+
+            //This is a design choice, although it might seem a bug. I want to be able
+            //to update observers of the sub-models without triggering events on super-models
+            //so potentially subviews can be redrawn without touching the main view.
+            expect(pChangeCallback).not.toHaveBeenCalled();
+
+            expect(pModel.hobby.list.length).toBe(1);
+            expect(pModel.hobby.list[0].hobby).toBe('free climbing');
+        });
     });
 
-    it('Creates a Model', function(){
-        var person = {
-            name: 'Max',
-            age: 30,
-            language: ['italian', 'english']
-        };
-        var model = OO.Model({model: person});
+    describe('Observer', function(){
 
-        expect(model instanceof OO.Model).toBe(true);
-        expect(model._state).toBeDefined();
-        expect(model.name).toBe('Max');
-        expect(model.age).toBe(30);
-        expect(model.language.length).toBe(2);
-    });
+        it('Observes a model waiting for change events', function(){
+            var changeCallback = jasmine.createSpy('Something changed!');
 
-    it('Triggers events on Models', function(){
-        var changeCallback = jasmine.createSpy('Something changed!');
+            var person = {
+                name: 'Max',
+                age: 30,
+                language: ['italian', 'english']
+            };
+            var model = OO.Model({model: person});
+            var observer = OO.Observer();
+            observer.observe(model, changeCallback);
+            model.age = 31;
 
-        var person = {
-            name: 'Max',
-            age: 30,
-            language: ['italian', 'english']
-        };
-        var model = OO.Model({model: person});
-        var cbid = model.on('change', changeCallback);
-        model.age = 31;
+            expect(changeCallback).toHaveBeenCalledWith('change', 'age');
+            changeCallback.reset();
 
-        expect(changeCallback).toHaveBeenCalledWith('change', 'age');
-        changeCallback.reset();
+            observer.unobserve(model);
+            model.age = 31;
 
-        model.language = ['italian', 'english', 'one day german'];
+            expect(changeCallback).not.toHaveBeenCalled();
+        });
 
-        expect(changeCallback).toHaveBeenCalledWith('change', 'language');
-        changeCallback.reset();
+        it('Observes multiple models waiting for change events', function(){
+            var changeCallback = jasmine.createSpy('Something changed!');
 
-        model.off('change', cbid);
-        model.age = 30;
+            var person = {
+                name: 'Max',
+                age: 30,
+                language: ['italian', 'english']
+            };
+            var otherPerson = {
+                name: 'John',
+                age: 28,
+                language: ['english']
+            };
+            var modelPerson = OO.Model({model: person});
+            var modelOtherPerson = OO.Model({model: otherPerson});
+            var observer = OO.Observer();
+            observer.observe(modelPerson, changeCallback);
+            observer.observe(modelOtherPerson, changeCallback);
 
-        expect(changeCallback).not.toHaveBeenCalled();
-    });
+            modelPerson.age = 31;
 
-    it('Refreshes the model by contacting the provided data source', function(){
-        var changeCallback = jasmine.createSpy('Something changed!');
+            expect(changeCallback).toHaveBeenCalledWith('change', 'age');
+            changeCallback.reset();
 
-        var person = {
-            name: 'Max',
-            age: 30,
-            language: ['italian', 'english']
-        };
-        var model = OO.Model({
-            model: person,
-            source: '/person/1234',
-            mapper: function(data){
-                //Response is of type
-                //{resonse: {my person}, meta:{info}}
-                return data.response;
-            }});
-        var cbid = model.on('change', changeCallback);
+            modelOtherPerson.language = ['spanish', 'english'];
 
-        model.refresh();
+            expect(changeCallback).toHaveBeenCalledWith('change', 'language');
+            changeCallback.reset();
 
-        expect(changeCallback.callCount).toBe(3);
-    });
+            observer.unobserve(modelPerson);
+            modelPerson.age = 31;
 
-    it('Observes models waiting for change events', function(){
-        var changeCallback = jasmine.createSpy('Something changed!');
+            expect(changeCallback).not.toHaveBeenCalled();
+            changeCallback.reset();
 
-        var person = {
-            name: 'Max',
-            age: 30,
-            language: ['italian', 'english']
-        };
-        var model = OO.Model({model: person});
-        var observer = OO.Observer();
-        observer.observe(model, changeCallback);
-        model.age = 31;
-
-        expect(changeCallback).toHaveBeenCalledWith('change', 'age');
-        changeCallback.reset();
-
-        observer.unobserve(model);
-        model.age = 31;
-
-        expect(changeCallback).not.toHaveBeenCalled();
+            modelOtherPerson.age = 29;
+            expect(changeCallback).toHaveBeenCalledWith('change', 'age');
+        });
     });
 });
